e5199cce7d5cb6b499c787d2fae9aae9d1a4da69
Koustubhpats23@gmail.com <Koustubhpats23@gmail.com>
2026-05-13 23:09:32 +0000

    Test2

--- models/example/staging_slvr/stg_customer.sql
+++ models/example/staging_slvr/stg_customer.sql
@@ -1,6 +1,6 @@
 WITH source AS (
 
-    SELECT * FROM {{ source('custom_reveal', 'customer') }}   -- ✅ correct referencesds
+    SELECT * FROM {{ source('custom_reveal', 'customer') }}   -- ✅ correct 
 
 ),
 
